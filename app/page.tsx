d939c4c99a9c84a39997ced7b4c744bd34e75657
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-11-14 22:40:28 +0700

    feat : homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,33 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 const HomePage = () => {
   return (
-    <div className="mt-20 px-20  w-fit">
-      <div>
-        <h1 className="text-5xl font-bold">
-          Streamline Your <br />
-          <span className="bg-gradient-to-r from-cyan-500 to-purple-500 bg-clip-text text-transparent">Workflow</span>
-        </h1>
-        <p className="w-90 mt-5">
-          Transform chaos into clarity with TaskFlow intuitive task management platform. Collaborate seamlessly and achieve more together.
-        </p>
-        <Button asChild className="bg-linear-to-r from-sky-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 text-white p-6 mt-5 rounded-lg font-semibold text-base 
-                                    transition-all duration-200 shadow-lg hover:shadow-xl transform hover:-translate-y-1">
-          <Link href="#">
-            Get Started Free<FaArrowRightLong />
-          </Link>
-        </Button>
+    <>
+      <div className="mt-20 px-20 flex w-fit justify-between gap-10">
+        <div>
+          <h1 className="title-content text-4xl">
+            Streamline Your <br />
+            <span className="bg-gradient-to-r from-cyan-500 to-purple-500 bg-clip-text text-transparent">Workflow</span>
+          </h1>
+          <p className="w-90 mt-5">
+            Transform chaos into clarity with TaskFlow intuitive task management platform. Collaborate seamlessly and achieve more together.
+          </p>
+          <Button asChild className="btn-linear">
+            <Link href="#">
+              Get Started Free<FaArrowRightLong />
+            </Link>
+          </Button>
+        </div>
+        <div>
+          <h1>Project Image Hover</h1>
+        </div>
       </div>
-      <div>
-
+      <div className="justify-items-center mt-30">
+        <div className="text-center">
+          <h1 className="subtitle-content">Everything you need to stay organized</h1>
+          <p className="text-gray-500">Powerful features designed to help teams collaborate efficiently and <br />deliver projects on time.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default HomePage;
