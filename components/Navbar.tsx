0cbd409128a3f4336c61af86d43291f14b2e04a5
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-11-14 15:54:09 +0700

    feat : navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link'
 import { ListTodo } from 'lucide-react';
+import { Button } from "@/components/ui/button"
 const Navbar = () => {
   return (
-    <nav className='flex justify-between p-4 bg-gray-200'>
+    <nav className='flex justify-between p-4 bg-gray-200 items-center'>
         <div className='flex'>
-            <Link href="/" className='flex'>
-                <ListTodo />TASKFLOW
+            <Link href="/" className='flex gap-2 font-bold text-xl items-center'>
+                <ListTodo/>TASKFLOW
             </Link>
         </div>
-        <div className='flex gap-4'>
+        <div className='flex gap-4 items-center'>
             <Link href=''>
                 Login
             </Link>
-            <Link href=''>
-                Sign Up
-            </Link>
+            <Button asChild className='bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-300'>
+                <Link href=''>Sign Up</Link>
+            </Button>
         </div>
     </nav>
   )
