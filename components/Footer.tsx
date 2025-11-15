7f3cc1a322d04682e45770415da43ee6c69cafa6
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-11-15 14:48:03 +0700

    feat : footer : fix size social media icon and hover

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,17 +7,17 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between p-4 px-20 bg-gray-100 items-center mt-10">
+    <footer className="flex justify-between p-4 px-20 items-center bg-linear-to-r from-blue-50 to-white">
         <div>
-            <Link href="/" className='flex gap-2 font-bold text-sm items-center'>
+            <Link href="/" className='flex gap-2 font-bold text-sm items-center text-gray-500'>
                 <ListTodo className='bg-linear-65 from-gray-300 to-gray-400 p-2 rounded-sm' size={30} color='#fff'/>TASKFLOW
             </Link>
         </div>
         <div className="flex gap-4 text-gray-500">
-            <FaTwitter />
-            <FaLinkedin />
-            <FaGithub />
-            <FaSquareInstagram />
+            <Link href="https://twitter.com"><FaTwitter size={20} className="hover:text-gray-700"/></Link>
+            <Link href="https://linkedin.com"><FaLinkedin size={20} className="hover:text-gray-700"/></Link>
+            <Link href="https://github.com"><FaGithub size={20} className="hover:text-gray-700"/></Link>
+            <Link href="https://instagram.com"><FaSquareInstagram size={20} className="hover:text-gray-700"/></Link>
         </div>
         <div className='text-center p-4 text-gray-500'>© 2025 TaskFlow. All rights reserved.</div>
     </footer>
