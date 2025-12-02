eca8fb0c8bc4d85b51083d0baf8025604d1268e2
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-12-02 14:57:09 +0700

    feat : login : create login page

---
+++ app/(public)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginPageView from "@/components/modules/login-page/view/login-page-view"
+
+const LoginPage = () => {
+  return (
+    <>
+        <LoginPageView />
+    </>
+  )
+}
+export default LoginPage
