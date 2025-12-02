0f3a6d216ad413183e530ba9d55b0236c035a510
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-12-02 17:11:49 +0700

    feat : login : fix input card login

--- components/modules/login-page/view/login-page-view.tsx
+++ components/modules/login-page/view/login-page-view.tsx
@@ -14,15 +14,15 @@ import { Label } from "@/components/ui/label";
 
 export default function LoginPageView() {
   return (
-    <div className="flex justify-center bg-gradient-to-r from-blue-50 to-white px-4">
+    <div className="flex justify-center px-4">
       <Card className="w-full max-w-sm">
-        <CardHeader className="justify-center">
+        <CardHeader className="justify-center py-10">
           <CardTitle className="text-3xl font-semibold flex gap-2">
             <ListTodo
               className="bg-gradient-to-br from-blue-500 to-purple-500 p-2 rounded-lg w-8 h-8 md:w-10 md:h-10"
               color="#fff"
             />
-            TASKFLOW
+            TaskFlow
           </CardTitle>
           {/* <CardDescription>
             Enter your email below to login to your account
@@ -33,7 +33,7 @@ export default function LoginPageView() {
         </CardHeader>
         <CardContent>
           <form>
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
               <div className="grid gap-2">
                 <Label htmlFor="email">
                   Email<span className="text-red-500">*</span>
@@ -51,7 +51,7 @@ export default function LoginPageView() {
                   href="/forgot-password"
                   className="ml-auto inline-block text-sm underline-offset-4 hover:underline text-blue-500!"
                 >
-                  Forgot your password?
+                  Forgot password?
                 </a>
               </div>
             </div>
@@ -61,13 +61,18 @@ export default function LoginPageView() {
           <Button type="submit" className="w-full" variant="loginBtn">
             Login
           </Button>
-          <hr className="w-full border-t border-gray-500" />
-          <a
-            href="/register"
-            className="inline-block text-lg underline-offset-4 hover:underline text-blue-500!"
-          >
-            Haven&apos;t an account?
-          </a>
+          <hr className="w-full border-t border-gray-300" />
+          <div className="flex gap-1 items-center justify-center">
+            <p className="text-sm lg:text-base text-gray-400 font-semibold">
+              Doesn&apos;t have an account ?
+            </p>
+            <a
+              href="/register"
+              className="text-sm lg:text-base underline-offset-4 hover:underline text-blue-600 font-semibold" 
+            >
+              Signup
+            </a>
+          </div>
           {/* <Button variant="outline" className="w-full">
             Login with Google
           </Button> */}
