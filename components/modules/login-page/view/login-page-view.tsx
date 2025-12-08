3c59481c7c532bced9b49f4cd226e0642bee82dd
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-12-08 11:23:25 +0700

    feat : login : validation login

--- components/modules/login-page/view/login-page-view.tsx
+++ components/modules/login-page/view/login-page-view.tsx
@@ -1,4 +1,20 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema, LoginFormValues } from "@/schemas/login"; // สร้างไฟล์นี้
+import { useRouter } from "next/navigation";
+// 2. นำเข้า Form Components จาก shadcn/ui
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage, // สำคัญมากสำหรับแสดง error
+} from "@/components/ui/form";
+
 import { ListTodo } from "lucide-react";
 import {
   Card,
@@ -13,6 +29,41 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function LoginPageView() {
+  const router = useRouter();
+  const users = [
+    {
+      email: "user@example.com",
+      password: "12345678",
+    },
+  ];
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: LoginFormValues) {
+    const user = users.find(
+      (user) => user.email === values.email && user.password === values.password
+    );
+
+    if (user) {
+      console.log("Login successful");
+      form.reset({ email: "", password: "" });
+      router.push("/dashboard");
+    } else {
+      form.setError("email", {
+        type: "manual",
+        message: "อีเมลหรือรหัสผ่านไม่ถูกต้อง",
+      });
+      form.setError("password", {
+        type: "manual",
+        message: "อีเมลหรือรหัสผ่านไม่ถูกต้อง",
+      });
+    }
+  }
   return (
     <div className="flex justify-center px-4">
       <Card className="w-full max-w-sm">
@@ -24,43 +75,75 @@ export default function LoginPageView() {
             />
             TaskFlow
           </CardTitle>
-          {/* <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription> */}
-          {/* <CardAction>
-            <Button variant="link">Sign Up</Button>
-          </CardAction> */}
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-3">
-              <div className="grid gap-2">
-                <Label htmlFor="email">
-                  Email<span className="text-red-500">*</span>
-                </Label>
-                <Input id="email" type="email" placeholder="" required />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">
-                    Password<span className="text-red-500">*</span>
-                  </Label>
-                </div>
-                <Input id="password" type="password" required />
-                <a
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline text-blue-500!"
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              {/* Email */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Email<span className="text-red-500">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="name@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Password */}
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center">
+                      <FormLabel>
+                        Password<span className="text-red-500">*</span>
+                      </FormLabel>
+                      <a
+                        href="/forgot-password"
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline text-blue-500"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                    <FormControl>
+                      <Input
+                        id="password"
+                        type="password"
+                        placeholder="••••••••"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                className="w-full mt-4"
+                variant="loginBtn"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? "กำลังเข้าสู่ระบบ..." : "Login"}
+              </Button>
+            </form>
+          </Form>
         </CardContent>
-        <CardFooter className="flex-col gap-5">
-          <Button type="submit" className="w-full" variant="loginBtn">
-            Login
-          </Button>
+
+        <CardFooter className="flex-col gap-5 pt-0">
           <hr className="w-full border-t border-gray-300" />
           <div className="flex gap-1 items-center justify-center">
             <p className="text-sm lg:text-base text-gray-400 font-semibold">
@@ -68,14 +151,11 @@ export default function LoginPageView() {
             </p>
             <a
               href="/register"
-              className="text-sm lg:text-base underline-offset-4 hover:underline text-blue-600 font-semibold" 
+              className="text-sm lg:text-base underline-offset-4 hover:underline text-blue-600 font-semibold"
             >
               Signup
             </a>
           </div>
-          {/* <Button variant="outline" className="w-full">
-            Login with Google
-          </Button> */}
         </CardFooter>
       </Card>
     </div>
--- schemas/login.ts
+++ schemas/login.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const LoginSchema = z.object({
   // Email: ต้องเป็น string, ต้องมี format เป็น email
   email: z.string().email({
-    message: "กรุณากรอก Email ที่ถูกต้อง",
+    message: "กรุณากรอกรูปแบบ Email ที่ถูกต้อง",
   }),
   // Password: ต้องเป็น string, และมีความยาวอย่างน้อย 8 ตัวอักษร
   password: z.string().min(8, {
