8d7518729b02a1ec28372bf7ea36b25b8547a007
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-12-08 10:48:16 +0700

    zod for validate

---
+++ schemas/login.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  // Email: ต้องเป็น string, ต้องมี format เป็น email
+  email: z.string().email({
+    message: "กรุณากรอก Email ที่ถูกต้อง",
+  }),
+  // Password: ต้องเป็น string, และมีความยาวอย่างน้อย 8 ตัวอักษร
+  password: z.string().min(8, {
+    message: "รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร",
+  }),
+});
+
+// กำหนด Type ของ Form Data เพื่อให้ Type-Safe
+export type LoginFormValues = z.infer<typeof LoginSchema>;
