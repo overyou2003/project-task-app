dce83c4ea862c3058c5b64fb98047a36f2deb10f
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2025-12-08 13:54:21 +0700

    feat : register : validation register

--- components/modules/register-page/view/register-page-view.tsx
+++ components/modules/register-page/view/register-page-view.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { ListTodo } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterFormValues, RegisterSchema } from "@/schemas/register";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -13,6 +27,39 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function RegisterPageView() {
+  const router = useRouter();
+  const [serverError, setServerError] = useState<string | null>(null);
+  const form = useForm<RegisterFormValues>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      fullName: "",
+      phoneNumber: "",
+    },
+  });
+
+  async function onSubmit(values: RegisterFormValues) {
+    setServerError(null);
+    console.log("Submitting Register data:", values);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      const success = true;
+
+      if (success) {
+        console.log("Registration successful!");
+        form.reset();
+        router.push("/login?registered=true");
+      } else {
+        const errorMessage = "This email is already registered.";
+        setServerError(errorMessage);
+      }
+    } catch (error) {
+      setServerError("Network error. Could not connect to the server.");
+    }
+  }
+
   return (
     <div className="flex justify-center">
       <Card className="w-full max-w-sm">
@@ -20,61 +67,128 @@ export default function RegisterPageView() {
           <CardTitle className="text-3xl text-center font-semibold flex gap-2">
             Create Your Account
           </CardTitle>
-          {/* <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription> */}
-          {/* <CardAction>
-            <Button variant="link">Sign Up</Button>
-          </CardAction> */}
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">
-                  Email<span className="text-red-500">*</span>
-                </Label>
-                <Input id="email" type="email" placeholder="" required />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">
-                    Password<span className="text-red-500">*</span>
-                  </Label>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="confirmPassword">
-                    Confirm Password<span className="text-red-500">*</span>
-                  </Label>
-                </div>
-                <Input id="confirmPassword" type="password" required />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="fullName">
-                    Full Name<span className="text-red-500">*</span>
-                  </Label>
-                </div>
-                <Input id="fullName" type="text" required />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="phoneNumber">
-                    Phone Number<span className="text-red-500">*</span>
-                  </Label>
-                </div>
-                <Input id="phoneNumber" type="text" required />
-              </div>
-            </div>
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              {/* Email */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Email<span className="text-red-500">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="name@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Password */}
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Password<span className="text-red-500">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Confirm Password */}
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Confirm Password<span className="text-red-500">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="confirmPassword"
+                        type="password"
+                        placeholder="Enter your password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Full Name */}
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Full Name<span className="text-red-500">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="fullName"
+                        type="text"
+                        placeholder="Enter your full name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Phone Number */}
+              <FormField
+                control={form.control}
+                name="phoneNumber"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Phone Number<span className="text-red-500">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        id="phoneNumber"
+                        type="text"
+                        placeholder="Enter your phone number"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full mt-4"
+                variant="registerBtn"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? "กำลังลงทะเบียน..." : "Submit"}
+              </Button>
+            </form>
+          </Form>
         </CardContent>
+
         <CardFooter className="flex-col gap-5">
-          <Button type="submit" className="w-full" variant="registerBtn">
-            Submit
-          </Button>
           <hr className="w-full border-t border-gray-300" />
           <a
             href="/login"
