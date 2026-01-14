4e5079cb372b4c7ab719698cb3243c7ac8463046
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2026-01-14 17:05:19 +0700

    feat: enhance registration process with email uniqueness check and error handling

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -8,11 +8,18 @@ export async function GET() {
 
 export async function POST(req: Request) {
   const { email, password, name } = await req.json();
-
+  const checkUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (checkUser) {
+    return Response.json({ error: "อีเมลนี้ถูกใช้งานแล้ว" }, { status: 400 });
+  }
   const hashed = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
-    data: { email, password: hashed, name }
+    data: { email, password: hashed, name },
   });
 
   return Response.json(user);
