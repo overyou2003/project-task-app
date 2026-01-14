906f335d6fc94b4643e607d6c3b70351af2a01f2
CLICKNEXT\Patiphat.B <patiphat.b@clicknext.co.th>
2026-01-14 17:56:29 +0700

    feat: implement login functionality with API integration and loading state

--- schemas/login.ts
+++ schemas/login.ts
@@ -2,9 +2,10 @@ import * as z from "zod";
 
 export const LoginSchema = z.object({
   // Email: ต้องเป็น string, ต้องมี format เป็น email
-  email: z.string().email({
-    message: "กรุณากรอกรูปแบบ Email ที่ถูกต้อง",
-  }),
+  email: z
+    .string()
+    .email("กรุณากรอกรูปแบบ Email ที่ถูกต้อง")
+    .transform((val) => val.toLowerCase()),
   // Password: ต้องเป็น string, และมีความยาวอย่างน้อย 8 ตัวอักษร
   password: z.string().min(8, {
     message: "รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร",
